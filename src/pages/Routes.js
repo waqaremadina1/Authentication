d6781daccdaef397899225e0b9abdf6a376f906d
Waqar Bin Abrar <31979294+waqaremadina1@users.noreply.github.com>
2024-12-31 17:46:11 +0500

    first commit

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -1,38 +1,38 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+// import React from 'react'
+// import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
-import Header from '../components/Header'
-import Home from './Home'
-import Add from './Extra/Add'
-import Read from './Extra/Read'
-import User from './Extra/User'
-import Login from './Login'
-import Register from './Register'
-import Footer from '../components/Footer'
+// import Header from '../components/Header'
+// import Home from './Home'
+// import Add from './Extra/Add'
+// import Read from './Extra/Read'
+// import User from './Extra/User'
+// import Login from './Login'
+// import Register from './Register'
+// import Footer from '../components/Footer'
 
-import NoPage from '../components/NoPage'
+// import NoPage from '../components/NoPage'
 
-export default function Index() {
-  return (
-    <BrowserRouter>
+// export default function Index() {
+//   return (
+//     <BrowserRouter>
 
-<Header />
-    <main>
-    <Routes>
+// <Header />
+//     <main>
+//     <Routes>
         
-        <Route path='/' element={<Home />} />
-        <Route path='/add' element={<Add />} />
-        <Route path='/read' element={<Read />} />
-        <Route path='/user' element={<User />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
+//         <Route path='/' element={<Home />} />
+//         <Route path='/add' element={<Add />} />
+//         <Route path='/read' element={<Read />} />
+//         <Route path='/user' element={<User />} />
+//         <Route path='/login' element={<Login />} />
+//         <Route path='/register' element={<Register />} />
 
-        <Route path='*' element={<NoPage />} />
+//         <Route path='*' element={<NoPage />} />
         
-    </Routes>
-    </main>
-    <Footer />
+//     </Routes>
+//     </main>
+//     <Footer />
 
-    </BrowserRouter>
-  )
-}
+//     </BrowserRouter>
+//   )
+// }
